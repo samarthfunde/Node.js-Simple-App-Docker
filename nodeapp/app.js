a159514dffbe28bd0c17fc827d1f486bc66348c5
Samarth Funde <122684505+samarthfunde@users.noreply.github.com>
2026-01-09 00:00:47 +0530

    Update app.js

--- nodeapp/app.js
+++ nodeapp/app.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = 3000;
 
 app.get("/", (req, res) => {
-  res.send("🚀 Node.js app running inside Docker!");
+  res.send("🚀 Node.js app running inside Docker!...");
 });
 
 app.listen(PORT, () => {
